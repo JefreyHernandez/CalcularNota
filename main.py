c310f274f5f3adee3dcd5a7039329e5c384cc1f8
JefreyHernandez <jefrey.hernandez.7e6@itb.cat>
2023-02-05 21:28:08 +0100

    Applying functions

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('a')
+import menuFunctions as mF
+
+mainMenu = ('UF', 'Modulo', 'Total', 'Salir')
+moduloMenu = ('M01 [Sistemas]', 'M02 [BBDD]', 'M03 [Programación]', 'M04 [Marcas]', 'M05 [Maquinas]', 'M07 [Redes]', 'M12 [FOL]', 'M13 [EIE]')
+ufMenu = (moduloMenu)
+
+mF.printOptions(mainMenu)
