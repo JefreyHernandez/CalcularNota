656e223fa516e96067b7eaa088d0e4ccef16dfcd
JefreyHernandez <jefrey.hernandez.7e6@itb.cat>
2023-02-25 03:04:29 +0100

    Variables of the Menu

---
+++ menus.py
@@ -0,0 +1,19 @@
+mainMenu = ('Calcular', 'Salir')
+calculateMenu = ('Módulo', 'UF', 'Total', 'Volver')
+signaturas = ('M01 [Sistemas]',
+              'M02 [BBDD]',
+              'M03 [Programación]',
+              'M04 [Marcas]',
+              'M05 [Maquinaria]',
+              'M06 [...]',
+              'M07 [Redes]',
+              'M08 [...]',
+              'M09 [...]',
+              'M10 [Gestores BBDD]',
+              'M11 [...]',
+              'M12 [FOL]',
+              'M13 [EIE]',
+              'Volver')
+
+menus = (mainMenu, calculateMenu, signaturas)
+titles = ('Main', 'Calcular Nota', 'Selección de asignatura')
